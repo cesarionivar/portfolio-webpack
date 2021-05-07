4db93d448b1e3e09a58a63b07b718dddd6a56a66
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-05-07 16:25:32 -0400

    Portfolio Hero done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,11 @@
-
 import mainPhoto from '../assets/main.png';
+
+import html from '../assets/html.png';
+import css from '../assets/css.svg';
+import javascript from '../assets/javascript.png';
+import typescript from '../assets/typescript.png';
+import webpack from '../assets/webpack.png';
+import react from '../assets/react.png';
 
 const Main = () => {
 
@@ -10,6 +16,14 @@ const Main = () => {
     <img src="${mainPhoto}" alt="main photo" class="main__img">
     <h1 class="main__name">Cesario Nivar</h1>
     <p class="main__title">Frontend Developer</p>
+    <div class="tecnicals">
+      <p class="tecnicals__html">html <img src="${html}" alt="html"></p>
+      <p class="tecnicals__css"><img src="${css}" alt="css"> css</p>
+      <p class="tecnicals__webpack">webpack <img src="${webpack}" alt="webpack"></p>
+      <p class="tecnicals__javascript"><img src="${javascript}" alt="javascript"> javascript </p>
+      <p class="tecnicals__typescript">typescript <img src="${typescript}" alt="typescript"></p>
+      <p class="tecnicals__react"><img src="${react}" alt="react"> react</p>
+    </div>
   `;
 
   return main;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/main.scss';
 import Header from './components/Header';
 import Main from './components/Main';
+// import TecnicalSkills from './components/TecnicalSkills';
 
 
 const app = document.getElementById('app');
 app.appendChild(Header());
 app.appendChild(Main());
+// app.appendChild(TecnicalSkills());
